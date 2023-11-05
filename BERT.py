4a44203b7254e2399bba0d52e5c54e85e0bfc1f5
sehyunsix <sdfg8931@ajou.ac.kr>
2023-11-05 19:44:19 +0900

    BERT 추가

---
+++ BERT.py
@@ -0,0 +1,29 @@
+  # BEGIN: 4j3d9f8hj3d9
+  import torch
+  from transformers import BertForTokenClassification, BertTokenizer
+
+  # Load the pre-trained BERT model and tokenizer
+  model_name = 'bert-base-uncased'
+  tokenizer = BertTokenizer.from_pretrained(model_name)
+  model = BertForTokenClassification.from_pretrained(model_name)
+
+  # Set the number of labels for the classification task
+  num_labels = 3
+
+  # Modify the pre-trained BERT model to output the correct number of labels
+  model.classifier = torch.nn.Linear(model.classifier.in_features, num_labels)
+
+  # Define the optimizer and learning rate
+  optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
+
+  # Define the loss function
+  loss_fn = torch.nn.CrossEntropyLoss()
+
+  # Example usage:
+  text = "Hello, my name is John."
+  tokens = tokenizer.tokenize(text)
+  inputs = tokenizer.encode(text, return_tensors="pt")
+  outputs = model(inputs).logits
+  predictions = torch.argmax(outputs, dim=2)
+  print([(token, label_list[prediction]) for token, prediction in zip(tokens, predictions[0].tolist())])
+  # END: 4j3d9f8hj3d9
